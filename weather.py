65d16af48c838339b86248b917793d9b967b5394
fanbinbin <fanbinbin@kmf.com>
2017-11-27 20:55:21 +0800

    table

--- weather.py
+++ weather.py
@@ -6,5 +6,5 @@ import requests
 url = "https://en.tutiempo.net/climate/01-2016/ws-591340.html"
 html = requests.get(url).content
 soup = BeautifulSoup(html)
-
-print(soup.decode('utf-8'))
+data = soup.find_all("table", class_="medias mensuales")
+print(data)
