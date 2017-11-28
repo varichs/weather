8eca83f6f8f9456e440ffd0ccbb1e94dfee1a036
fanbinbin <fanbinbin@kmf.com>
2017-11-28 10:19:14 +0800

    test

--- weather.py
+++ weather.py
@@ -7,6 +7,8 @@ url = "https://en.tutiempo.net/climate/01-2016/ws-591340.html"
 html = requests.get(url).content
 soup = BeautifulSoup(html, "html.parser")
 data = soup.find("table", "medias mensuales")
-rows = data.find("tr")
+rows = data.find_all("tr")
+for row in rowa.find_all("td")
+    print(row.text)
 
 print(rows)
