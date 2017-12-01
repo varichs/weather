7246aa7bfa9194b2f696d9af16a4e8e30be6cc71
fanbinbin <fanbinbin@kmf.com>
2017-12-01 13:18:31 +0800

    test

--- weather.py
+++ weather.py
@@ -63,9 +63,9 @@ def get_data(year, month):
     csv_writer(data, file_name)
 
 
-years = ['2015', '2016']
+years = ['2016']
 # month = [str(i) for i in range(1, 13)]
-months = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']
+months = [ '04', '05', '06', '07', '08', '09', '10', '11', '12']
 
 for year in years:
     for month in months:
